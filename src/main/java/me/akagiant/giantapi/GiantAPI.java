edf1cec4357b5d0f62d41602630f8be4ba92f97d
AkaGiant <AkaGiant01@gmail.com>
2022-07-20 20:39:47 +0100

    Initial Commit

---
+++ src/main/java/me/akagiant/giantapi/GiantAPI.java
@@ -0,0 +1,17 @@
+package me.akagiant.giantapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GiantAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
